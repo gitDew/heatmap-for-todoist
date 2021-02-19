bc7b7f01a5b3ebb59a79dc8f94bfc0e6dd6e2be2
gitDew <krisztian.harmat@gmail.com>
2021-02-19 01:14:19 +0100

    Background script throws errors correctly

--- background.ts
+++ background.ts
@@ -1,9 +1,16 @@
 import { fetchAndUpdate } from "./todoist";
+import { getToken, saveToken, checkForToken} from "./storage";
 
-chrome.runtime.onStartup.addListener(() => {
-    fetchAndUpdate();
-});
-  
 chrome.webNavigation.onCompleted.addListener(function() {
-    fetchAndUpdate();
+  checkForToken()
+    .then((hasToken: boolean) => {
+      if (hasToken) {
+        fetchAndUpdate()
+          .catch((error) => {
+            console.error("Failed to fetch and update todoist tasks in the background. Reason: " + error.message)
+          })
+      } else {
+        console.log("No token found in storage. Tasks will not be fetched.")
+      }
+    })
 }, {url: [{urlMatches : 'https://en.todoist.com/'}]});
